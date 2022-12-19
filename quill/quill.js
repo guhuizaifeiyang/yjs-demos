c0944be6a4317f604f1657bdfbedc5af0d9c7b0a
guhuizaifeiyang <648808308@qq.com>
2022-12-19 14:51:21 +0800

    扩展编辑器功能

--- quill/quill.js
+++ quill/quill.js
@@ -1,60 +1,75 @@
 /* eslint-env browser */
 
 import * as Y from 'yjs'
-import { WebsocketProvider } from 'y-websocket'
-import { QuillBinding } from 'y-quill'
+import {WebsocketProvider} from 'y-websocket'
+import {QuillBinding} from 'y-quill'
 import Quill from 'quill'
 import QuillCursors from 'quill-cursors'
-import './quill.css'
 
 Quill.register('modules/cursors', QuillCursors)
 
+console.log('This is js!!!')
 window.addEventListener('load', () => {
-  const ydoc = new Y.Doc()
-  const provider = new WebsocketProvider('wss://demos.yjs.dev', 'quill-demo-4', ydoc)
-  const ytext = ydoc.getText('quill')
-  const editorContainer = document.createElement('div')
-  editorContainer.setAttribute('id', 'editor')
-  document.body.insertBefore(editorContainer, null)
+    const ydoc = new Y.Doc()
+    const provider = new WebsocketProvider('wss://demos.yjs.dev', 'quill-demo-chandler', ydoc)
+    const ytext = ydoc.getText('quill')
+    const editorContainer = document.createElement('div')
+    editorContainer.setAttribute('id', 'editor')
+    document.body.insertBefore(editorContainer, null)
 
-  const editor = new Quill(editorContainer, {
-    modules: {
-      cursors: true,
-      toolbar: [
-        [{ header: [1, 2, false] }],
-        ['bold', 'italic', 'underline'],
-        ['image', 'code-block']
-      ],
-      history: {
-        userOnly: true
-      }
-    },
-    placeholder: 'Start collaborating...',
-    theme: 'snow' // or 'bubble'
-  })
+    var toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
 
-  const binding = new QuillBinding(ytext, editor, provider.awareness)
+        [{'header': 1}, {'header': 2}],               // custom button values
+        [{'list': 'ordered'}, {'list': 'bullet'}],
+        [{'script': 'sub'}, {'script': 'super'}],      // superscript/subscript
+        [{'indent': '-1'}, {'indent': '+1'}],          // outdent/indent
+        [{'direction': 'rtl'}],                         // text direction
 
-  /*
-  // Define user name and user name
-  // Check the quill-cursors package on how to change the way cursors are rendered
-  provider.awareness.setLocalStateField('user', {
-    name: 'Typing Jimmy',
-    color: 'blue'
-  })
-  */
+        [{'size': ['small', false, 'large', 'huge']}],  // custom dropdown
+        [{'header': [1, 2, 3, 4, 5, 6, false]}],
 
-  const connectBtn = document.getElementById('y-connect-btn')
-  connectBtn.addEventListener('click', () => {
-    if (provider.shouldConnect) {
-      provider.disconnect()
-      connectBtn.textContent = 'Connect'
-    } else {
-      provider.connect()
-      connectBtn.textContent = 'Disconnect'
-    }
-  })
+        [{'color': []}, {'background': []}],          // dropdown with defaults from theme
+        [{'font': []}],
+        [{'align': []}],
 
-  // @ts-ignore
-  // window.example = { provider, ydoc, ytext, binding, Y }
+        ['clean']                                         // remove formatting button
+    ];
+    const editor = new Quill(editorContainer, {
+        modules: {
+            cursors: true,
+            toolbar: toolbarOptions,
+            history: {
+                userOnly: true
+            }
+        },
+        placeholder: 'Start collaborating...',
+        theme: 'snow' // or 'bubble'
+    })
+
+    const binding = new QuillBinding(ytext, editor, provider.awareness)
+
+    /*
+    // Define user name and user name
+    // Check the quill-cursors package on how to change the way cursors are rendered
+    provider.awareness.setLocalStateField('user', {
+      name: 'Typing Jimmy',
+      color: 'blue'
+    })
+    */
+
+    const connectBtn = document.getElementById('y-connect-btn')
+    connectBtn.addEventListener('click', () => {
+        if (provider.shouldConnect) {
+            provider.disconnect()
+            connectBtn.textContent = 'Connect'
+        } else {
+            provider.connect()
+            connectBtn.textContent = 'Disconnect'
+        }
+    })
+
+    // @ts-ignore
+    // window.example = { provider, ydoc, ytext, binding, Y }
 })
--- quill/webpack.config.js
+++ quill/webpack.config.js
@@ -1,38 +1,44 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  entry: {
-    quill: './quill.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Quill Demo',
-      template: 'quill-online.html'
-    }),
-    new MiniCssExtractPlugin()
-  ],
-  output: {
-    globalObject: 'self',
-    path: path.resolve(__dirname, './dist/'),
-    filename: '[name].bundle.js',
-    // publicPath: '/quill/dist/',
-    clean: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
+    mode: 'development',
+    devtool: 'eval-cheap-module-source-map',
+    entry: {
+        quill: './quill.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Quill Demo',
+            template: 'quill-online.html'
+        }),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {from: path.resolve(__dirname, 'quill.css'), to: path.resolve(__dirname, 'dist/quill.css')},
+            ],
+        }),
     ],
-  },
-  devServer: {
-    contentBase: path.join(__dirname),
-    compress: true,
-    publicPath: '/dist/'
-  }
+    output: {
+        globalObject: 'self',
+        path: path.resolve(__dirname, './dist/'),
+        filename: '[name].bundle.js',
+        // publicPath: '/quill/dist/',
+        clean: true
+    },
+    /*module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },*/
+    devServer: {
+        static: path.join(__dirname),
+        compress: true,
+        // publicPath: '/dist/'
+    }
 }
