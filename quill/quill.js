8b7479bfd31bde0b0bde61aad14e7607bf9833b7
guhuizaifeiyang <648808308@qq.com>
2023-03-16 10:36:33 +0800

    添加check工具栏

--- quill/quill.js
+++ quill/quill.js
@@ -11,7 +11,7 @@ var toolbarOptions = [
     ['blockquote', 'code-block'],
 
     [{'header': 1}, {'header': 2}],               // custom button values
-    [{'list': 'ordered'}, {'list': 'bullet'}],
+    [{'list': 'ordered'}, {'list': 'bullet'}, {'list': 'check'}],
     [{'script': 'sub'}, {'script': 'super'}],      // superscript/subscript
     [{'indent': '-1'}, {'indent': '+1'}],          // outdent/indent
     [{'direction': 'rtl'}],                         // text direction
